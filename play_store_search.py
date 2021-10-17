e04f49822c877e88c5097e1a8a04c3e1d65c9bd7
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2021-10-17 22:23:11 +0530

    Searching any app in Play Store by navigating to the top result

--- play_store_search.py
+++ play_store_search.py
@@ -5,6 +5,11 @@ browser = wd.Chrome()
 browser.maximize_window()
 browser.get("https://play.google.com/store")
 
-browser.find_elements_by_name("q")[0].send_keys("WhatsApp")
+app_name = input("Which app you want to open?")
+
+browser.find_elements_by_name("q")[0].send_keys(app_name)
 browser.find_elements_by_tag_name("button")[0].send_keys(Keys.ENTER)
 
+browser.refresh()
+
+browser.execute_script("document.getElementsByClassName('WsMG1c nnK0zc')[0].click()") # Selecting the top result
